4b90820c4bda70a5efc89dfcc962b98ed6a6f9b3
Jagadish <jagadish@Jagadishs-MacBook-Pro-2.local>
2023-07-20 18:13:13 +0530

    First commit

---
+++ R/hello.R
@@ -0,0 +1,17 @@
+#' @title Diagnosis of Metabolic Syndrome
+#'
+#' @description
+#'
+#'
+#' @param x the name of the person to say hi to
+#'
+#' @return the output from code
+#' @export
+#'
+#' @examples
+#' mets("Jagadish")
+
+
+ mets <- function(x) {
+  print(paste0("Hello ",x,", how are you?"))
+}
