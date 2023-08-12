c63fa5c32d938e69a95b609edbdb4b5bea44f624
Jagadish <jagadish@Jagadishs-MacBook-Pro-2.local>
2023-08-12 12:03:47 +0530

    pkg dwn

--- R/data.R
+++ R/data.R
@@ -2,7 +2,7 @@
 #'
 #' @description A data frame with exact column names as specified.
 #'
-#' @format A data frame with parameters needed to diganose metabolic syndrome.
+#' @format A data frame with parameters needed to diagnose metabolic syndrome.
 #' \describe{
 #'   \item{Gender}{Gender in Male or Female}
 #'   \item{WC}{Wasit Circumference in cm}
